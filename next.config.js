86bcd6705fa552caa4899893ec0ed5ae1ef6fad8
imnyang <me@imnyang.xyz>
2023-12-21 21:36:45 +0900

    test

--- next.config.js
+++ next.config.js
@@ -1,10 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.md$/,
+      use: "raw-loader",
+    });
+    return config;
+  },
   output: "export",
   images: {
     unoptimized: true
   }
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
