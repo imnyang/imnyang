50ff5f7125a7ddc93519e8101d96eb78280695fa
imnyang <me@imnyang.xyz>
2024-01-07 14:53:15 +0900

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,6 +11,7 @@ export default function App({ Component, pageProps }: AppProps) {
           name="description"
           content="평범한 한 학생의 포트폴리오"
         />
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4578100060747818" crossorigin="anonymous"></script>
       </Head>
       <Component {...pageProps} />
     </>
