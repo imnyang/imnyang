adbcab93c9821ce25b54f269cfc57377aef58494
imnyang <me@imnya.ng>
2025-01-09 13:55:59 +0900

    급한불

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -7,8 +7,8 @@ import { TimeCounter } from "~/components/TimeCounter";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "암냥" },
+    { name: "description", content: "Student Developer" },
   ];
 }
 
@@ -26,13 +26,24 @@ export default function Home() {
   return (
     <div ref={ref} className="flex flex-col w-full items-center">
       <header className="w-full md:w-2/5 h-auto flex flex-row justify-between p-5">
-        <Link to="/" accessKey="h" title="💕 Alt + H" className="text-2xl">💕</Link>
+        <Link to="/" accessKey="h" title="💕 Alt + H" className="text-2xl">
+          💕
+        </Link>
         <div className="flex items-center">
           <AnimatedTabs activeTab={activeTab} setActiveTab={setActiveTab} />
         </div>
       </header>
-      <div id="top" className="w-auto text-center flex items-center justify-center flex-col gap-4">
-        <img src="https://f.imnya.ng/profile/34b47ba35448cc74a659bcec443c3fbc.webp" alt="imnyang" width={200} height={200} className="rounded-full" />
+      <div
+        id="top"
+        className="w-auto text-center flex items-center justify-center flex-col gap-4"
+      >
+        <img
+          src="https://f.imnya.ng/profile/34b47ba35448cc74a659bcec443c3fbc.webp"
+          alt="imnyang"
+          width={200}
+          height={200}
+          className="rounded-full"
+        />
         <div>
           <h1 className="text-2xl font-bold">암냥</h1>
           <p className="text-sm text-neutral-400">@imnyang</p>
@@ -40,7 +51,6 @@ export default function Home() {
         <TimeCounter />
         <h2 className="text-xl">{`Tab: ${activeTab}`}</h2>
       </div>
-
     </div>
   );
 }
