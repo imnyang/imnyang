14a04aa3e7a1dc3a43138ae3e63e44cd6ee60069
imnyang <imnyang@proton.me>
2025-05-08 16:40:17 +0900

    아 404 컴포넌트 날먹 개꿀이죠~

--- src/app/utils/NotFound.tsx
+++ src/app/utils/NotFound.tsx
@@ -1,8 +1,17 @@
+import { CommitsGrid } from "@/components/commits-grid"
+import { Button } from "@/components/ui/button";
+import { useNavigate } from "react-router";
+
 export default function NotFound() {
+    const navigate = useNavigate();
+
     return (
-        <div>
-            <h1>404 - Not Found</h1>
-            <p>The page you are looking for does not exist.</p>
+        <div className="flex flex-col justify-center items-center h-screen gap-8">
+            <CommitsGrid text="404" />
+            <p className="text-2xl">The page you are looking for does not exist.</p>
+            <Button variant="outline" onClick={() => navigate("/")}>
+                Go to Home
+            </Button>
         </div>
     );
 }
